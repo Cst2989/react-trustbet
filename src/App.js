31b309d764c8cf5498468db405d71c6b15b65eb7
Neciu Dan <dan@adoreme.com>
2020-03-24 15:48:43 +0200

    added web3-react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { useWeb3React } from '@web3-react/core';
 function App() {
+  const web3React = useWeb3React()
   return (
     <div className="App">
       <header className="App-header">
