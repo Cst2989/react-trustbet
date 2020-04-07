61e82332b50493875b7187e2a56e37a150936a5c
Neciu Dan <dan@adoreme.com>
2020-04-07 14:25:21 +0300

    Updates

---
+++ precache-manifest.4b2214610a53ac6274a2882570d25172.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9a016992fac3f20ae4f6ac36ee3bc5",
+    "url": "/react-trustbet/index.html"
+  },
+  {
+    "revision": "740e795d2747befa3021",
+    "url": "/react-trustbet/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ea7044a9fc5113d29cf",
+    "url": "/react-trustbet/static/js/2.0145451b.chunk.js"
+  },
+  {
+    "revision": "deea066462e3852fb7f610c3fa67aa20",
+    "url": "/react-trustbet/static/js/2.0145451b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740e795d2747befa3021",
+    "url": "/react-trustbet/static/js/main.48ebd80c.chunk.js"
+  },
+  {
+    "revision": "bd00c89318449b14800d",
+    "url": "/react-trustbet/static/js/runtime-main.58b43f0e.js"
+  }
+]);
