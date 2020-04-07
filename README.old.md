2076e8112d574f95731510e0b4354fdeb86af1b2
Neciu Dan <dan@adoreme.com>
2020-04-07 14:21:02 +0300

    Added login with metamask

--- README.old.md
+++
@@ -1,3 +0,0 @@
-# react-trustbet
-
-A FE application for a blockchain aplication
--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Web3 from 'web3';
 import './App.css';
-import { useWeb3React } from '@web3-react/core';
 function App() {
-  const web3React = useWeb3React()
+  const [adress, setAdress] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+  const handleClick = () => {
+    
+    try { 
+        window.ethereum.enable().then(function() {
+          const web3 = new Web3(window.ethereum);
+          setLoggedIn(true)
+          web3.eth.getAccounts().then(address => {
+            setAdress(address);
+          });
+        });
+    } catch(e) {
+        setLoggedIn(false)
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {
+            loggedIn ? <div><h2>Adress: </h2> { adress } </div>:
+          <button onClick={handleClick}>Login with Metamask</button>
+          }
       </header>
     </div>
   );
